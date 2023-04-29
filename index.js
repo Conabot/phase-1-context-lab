731071b9bf9d08cae2880c3911676278e10317d2
Minh Tran <tdaimike@gmail.com>
2023-04-29 00:25:36 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,5 +1,72 @@
 /* Your Code Here */
 
+
+function createEmployeeRecord(employeeInfoArray) {
+    return {
+      firstName: employeeInfoArray[0],
+      familyName: employeeInfoArray[1],
+      title: employeeInfoArray[2],
+      payPerHour: employeeInfoArray[3],
+      timeInEvents: [],
+      timeOutEvents: []
+    };
+  }
+
+
+  function createEmployeeRecords(employeeInfoArrays) {
+    return employeeInfoArrays.map(function(employeeInfoArray) {
+      return createEmployeeRecord(employeeInfoArray);
+    });
+  }
+  
+
+  function createTimeInEvent(dateTime) {
+    const [date, hour] = dateTime.split(" ")
+  
+    this.timeInEvents.push({
+      type: "TimeIn",
+      hour: parseInt(hour, 10),
+      date,
+    })
+  
+    return this;
+  }
+
+  
+function createTimeOutEvent(dateTime) {
+  const [date, hour] = dateTime.split(' ');
+
+  this.timeOutEvents.push({
+    type: "TimeOut",
+    hour: parseInt(hour, 10),
+    date,
+  });
+
+  return this;
+}
+
+function hoursWorkedOnDate(date) {
+  const timeIn = this.timeInEvents.find(function (event) {
+    return event.date === date
+  })
+
+  const timeOut = this.timeOutEvents.find(function (event) {
+    return event.date === date
+  })
+
+  if (timeIn && timeOut) {
+    return (timeOut.hour - timeIn.hour) / 100
+  } else {
+    return 0
+  }
+}
+
+function wagesEarnedOnDate(date) {
+  const hoursWorked = hoursWorkedOnDate.call(this, date)
+  const payRate = this.payPerHour
+
+  return hoursWorked * payRate
+}
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -21,3 +88,14 @@ const allWagesFor = function () {
     return payable
 }
 
+function findEmployeeByFirstName(srcArray, firstName) {
+  return srcArray.find(function (employee) {
+    return employee.firstName === firstName
+  })
+}
+
+function calculatePayroll(employeeRecords) {
+  return employeeRecords.reduce(function (totalPay, employee) {
+    return totalPay + allWagesFor.call(employee)
+  }, 0)
+}
